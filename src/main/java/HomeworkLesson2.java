d6b055a0b2023a845ea431c3f042b03a0beb001a
klont <klont@sii.pl>
2022-08-10 08:53:50 +0200

    Add HomeworkLesson2, pom.xml files

---
+++ src/main/java/HomeworkLesson2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class HomeworkLesson2 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(tokenGenerator(random, (userTokenLengthChoiceAndValidation())));
+
+    }
+
+    public static String tokenGenerator(Random random, int length) {
+        return random.ints(33, 126)
+                .mapToObj(i -> (char) i)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+
+    public static int userTokenLengthChoiceAndValidation() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert the token length from the following numbers: 5, 10 or 15: ");
+        String chosenNumber = scanner.nextLine();
+
+        String[] correctLengthNumber = new String[]{"5", "10", "15"};
+        List<String> lengthNumberArray = new ArrayList<>((Arrays.asList(correctLengthNumber)));
+
+        while (!lengthNumberArray.contains(chosenNumber)) {
+            System.out.println("Please insert correct value: ");
+            chosenNumber = scanner.nextLine();
+        }
+        return Integer.parseInt(chosenNumber);
+    }
+
+}
